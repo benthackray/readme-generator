3fc6201c5a0ccb96bd5e7d3ab57dba4aa7db8fb7
Ben Thackray <mthack92@gmail.com>
2021-06-24 18:56:53 -0400

    added table of contents

--- index/index.js
+++ index/index.js
@@ -7,6 +7,14 @@ const generateREADME = (data) =>
 
 ${data.description}
 
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
 ## Installation
 
 ${data.installation}
