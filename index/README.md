4649147194bb31f715d79a1ec325974c5766ca9f
Ben Thackray <mthack92@gmail.com>
2021-06-24 18:17:58 -0400

    added prompts, function, inititalized npm and added inquirer

---
+++ index/README.md
@@ -0,0 +1,29 @@
+# test
+
+this is a test
+
+## Installation
+
+no installation, just a test
+
+## Usage
+
+it can't. it's a test
+
+## License
+
+MIT License
+
+## Contributing
+
+no rules, just test
+
+## Tests
+
+this is litterally the test
+
+## Questions
+
+If you have any questions, please send them to test@email.com
+
+GitHub: [test](github.com/test)
--- index/index.js
+++ index/index.js
@@ -1,1 +1,104 @@
-console.log("Hello World");
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = (data) =>
+`# ${data.title}
+
+${data.description}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions, please send them to ${data.email}
+
+GitHub: [${data.github}](github.com/${data.github})
+`;
+
+// TODO: Create an array of questions for user input
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a short description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions for your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How can this project be used?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license will you use for this project?',
+            choices: ['MIT License', 'Apache License v2.0', 'GNU General Public License v3.0']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'What are the rules for contributing to this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What kinds of tests are to be used?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter an email address to recieve questions:',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username:',
+        },
+
+    ])
+    .then((data)=> {
+        const readmeContent = generateREADME(data)
+
+        fs.writeFile('README.md', readmeContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+        )
+    }
+    );
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+// function init() { }
+
+// Function call to initialize app
+// init();
